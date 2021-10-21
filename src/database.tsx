b33ec8b03f51439455d3b3dbf282a31b24b3c5a1
Per Nielsen Tikær <pnt@klean.dk>
2021-10-21 12:16:30 +0200

    Added new logo and new structure

--- src/database.tsx
+++ src/database.tsx
@@ -1,33 +1,34 @@
-import { ActionPanel, CopyToClipboardAction, FileIcon, Icon, List, showHUD } from "@raycast/api";
+import {ActionPanel, List, OpenInBrowserAction} from "@raycast/api";
 import plist from "plist";
 import fs from "fs";
 import os from "os";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 export default function DatabaseList() {
 	const [state, setState] = useState<Item[]>([]);
 	const [query, setQuery] = useState<string | undefined>(undefined);
-	const tablePlusLocation = `${os.homedir()}/Library/Application\ Support/com.tinyapp.TablePlus/Data/Connections.plist`;
+	const [loading, setLoading] = useState(false)
+	const tablePlusLocation = `${os.homedir()}/Library/Application Support/com.tinyapp.TablePlus/Data/Connections.plist`;
 
 	useEffect(() => {
+		try {
+			setLoading(true)
+			const connections = plist.parse(fs.readFileSync(tablePlusLocation, "utf8")) as ReadonlyArray<plist.PlistObject>;
 
-			const obj = plist.parse(fs.readFileSync(tablePlusLocation, "utf8"));
-			obj.children.map((line) => {
-
-				return {
-					line.ID,
-					line.ConnectionName,
-					line.GroupID
-				}
+			const obj = connections.map((connection) => {
+				return {id: connection.ID, groupName: connection.GroupID, name: connection.ConnectionName} as Item;
 			})
+			setLoading(false)
 
 			setState(obj);
-
+		} catch (error) {
+			console.log(error)
+		}
 	}, []);
 
 	return (
 		<List
-			isLoading={state.length === 0}
+			isLoading={loading}
 			searchBarPlaceholder="Filter by name..."
 			onSearchTextChange={(query) => setQuery(query)}
 		>
@@ -45,8 +46,9 @@ export default function DatabaseList() {
 							icon='/Applications/TablePlus.app/Contents/Resources/AppIcon.icns'
 							actions={
 								<ActionPanel>
-
+									<OpenInBrowserAction title="Open database" url={`tableplus://?id=${process.id}`}/>
 								</ActionPanel>
+
 							}
 						/>
 					);
